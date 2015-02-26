99c94033a81cb71b1815dfb4840b6fb2e4fdbfaa
John St. Clair <stclairjohn@gmail.com>
2015-02-26 12:54:57 +0100

    [NPM] Implement bump

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,12 +7,17 @@ var gulp = require('gulp'),
     buffer = require('vinyl-buffer'),
     util = require('gulp-util'),
     runSequence = require('run-sequence').use(gulp),
-    del = require('del');
+    del = require('del'),
+    fs = require('fs'),
+    semver = require('semver'),
+    argv = require('yargs').argv;
 
 var config = {
     source: ['./src/editableCell.js'],
     target: './out',
-    externals: ['knockout', 'jquery']
+    externals: ['knockout', 'jquery'],
+    increment: argv.inc || 'patch',
+    dryRun: argv.dryrun || false
 };
 
 gulp.task('default', function(done) {
@@ -94,3 +99,17 @@ gulp.task('_test', function(done) {
 gulp.task('test', function(done) {
     return runSequence('_bundle_tests', ['_test'], done);
 });
+
+gulp.task('bump', function() {
+    var pkg = getPackageJson(),
+        newVer = semver.inc(pkg.version, config.increment);
+
+    return gulp.src('./package.json')
+        .pipe($.bump({ version: newVer }))
+        .pipe(gulp.dest('./'))
+        .pipe($.if(!config.dryRun, $.git.commit('Bumped version to v' + newVer)));
+});
+
+function getPackageJson() {
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+}
